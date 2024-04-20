ff7d78a99c6ffe724740d32aea50c9ef9f0bed77
Mityushkin <ndmityushkin@sberbank.ru>
2024-04-20 10:21:29 +0700

    init service

---
+++ src/test/kotlin/ru/mityushkin/medicationmanagementbackend/MedicationManagementBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.mityushkin.medicationmanagementbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedicationManagementBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
